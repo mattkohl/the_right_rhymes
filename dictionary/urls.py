c38ad382ed30f6fa7936cfedfa2a8896a11c253a
mattkohl <hlaford@gmail.com>
2016-01-09 20:30:20 +0000

    Fixed URL resolution bug

--- dictionary/urls.py
+++ dictionary/urls.py
@@ -15,15 +15,15 @@ urlpatterns = [
     url(r'^$', views.index, name="dictionary_index"),
 
     # /headword-as-a-slug/
-    url(r'^(?P<headword_slug>[a-zA-Z0-9\-_#]+)/?$', views.entry, name='entry'),
+    url(r"^(?P<headword_slug>[a-zA-Z0-9\-_#’']+)/?$", views.entry, name='entry'),
 
     # /artist-name-as-a-slug/
-    url(r"^artists/(?P<artist_slug>[a-zA-Z0-9\-_',\(\)]+)/$", views.artist, name='artist'),
+    url(r"^artists/(?P<artist_slug>[a-zA-Z0-9\-_'’,\(\)]+)/$", views.artist, name='artist'),
+
+    # /domain-as-a-slug/
+    url(r"^domains/(?P<domain_slug>[a-zA-Z0-9\-_’']+)/$", views.domain, name='domain'),
 
     # /named-entity-as-a-slug/
-    url(r'^domains/(?P<domain_slug>[a-zA-Z0-9\-_]+)/$', views.domain, name='domain'),
-
-    # /named-entity-as-a-slug/
-    url(r'^entities/(?P<entity_slug>[a-zA-Z0-9\-_]+)/$', views.entity, name='entity'),
+    url(r"^entities/(?P<entity_slug>[a-zA-Z0-9\-_'’]+)/$", views.entity, name='entity'),
 
 ]
