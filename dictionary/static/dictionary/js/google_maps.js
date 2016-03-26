2716c21ac1d2bea370e899e8da0d207d1d0ab3fc
mattkohl <hlaford@gmail.com>
2016-03-26 07:06:58 +0000

    included message on search results

--- dictionary/static/dictionary/js/google_maps.js
+++ dictionary/static/dictionary/js/google_maps.js
@@ -1,43 +1,44 @@
 /**
  * Created by MBK on 13/01/16.
  */
-$(function(){
-    function initialize() {
+$(document).ready(function() {
+    $(function () {
+        function initialize() {
 
-        var endpoint;
-        var latlng = new google.maps.LatLng(40.650002, -73.949997);
-        var isDraggable = !('ontouchstart' in document.documentElement);
-        var options = {
-            zoom: 10,
-            center: latlng,
-            mapTypeId: google.maps.MapTypeId.ROADMAP,
-            scrollwheel: false,
-            draggable: isDraggable
-          };
+            var endpoint;
+            var latlng = new google.maps.LatLng(40.650002, -73.949997);
+            var isDraggable = !('ontouchstart' in document.documentElement);
+            var options = {
+                zoom: 10,
+                center: latlng,
+                mapTypeId: google.maps.MapTypeId.ROADMAP,
+                scrollwheel: false,
+                draggable: isDraggable
+            };
 
-        $.each($('.map-canvas'), function(i) {
-            var index = i+1;
-            var map = new google.maps.Map(document.getElementById('map' + index), options);
-            var markers = [];
-            var mc = new MarkerClusterer(map);
-            var is_entry = true;
-            var infowindow = new google.maps.InfoWindow({ content: '' });
-            var markerBounds = new google.maps.LatLngBounds();
-            var sense_id = $(this).find('.sense_id').text();
-            var artist_slug = $(this).find('.artist-slug').text();
-            var place_slug = $(this).find('.place-slug').text();
-            if (artist_slug) {
-                endpoint = '/artists/' + artist_slug + '/json/';
-                is_entry = false;
-            } else if (place_slug) {
-                endpoint = '/places/' + place_slug + '/latlng/';
-                is_entry = false;
-            } else {
-                endpoint = '/senses/' + sense_id + '/artists/json/';
-            }
-            $.getJSON(endpoint, { 'csrfmiddlewaretoken': '{{csrf_token}}' }, function(data) {
+            $.each($('.map-canvas'), function (i) {
+                var index = i + 1;
+                var map = new google.maps.Map(document.getElementById('map' + index), options);
+                var markers = [];
+                var mc = new MarkerClusterer(map);
+                var is_entry = true;
+                var infowindow = new google.maps.InfoWindow({content: ''});
+                var markerBounds = new google.maps.LatLngBounds();
+                var sense_id = $(this).find('.sense_id').text();
+                var artist_slug = $(this).find('.artist-slug').text();
+                var place_slug = $(this).find('.place-slug').text();
+                if (artist_slug) {
+                    endpoint = '/artists/' + artist_slug + '/json/';
+                    is_entry = false;
+                } else if (place_slug) {
+                    endpoint = '/places/' + place_slug + '/latlng/';
+                    is_entry = false;
+                } else {
+                    endpoint = '/senses/' + sense_id + '/artists/json/';
+                }
+                $.getJSON(endpoint, {'csrfmiddlewaretoken': '{{csrf_token}}'}, function (data) {
                     parsed = $.parseJSON(data);
-                    $.each(parsed.places, function(index, p) {
+                    $.each(parsed.places, function (index, p) {
                         if (p != null && p.latitude) {
                             tmpLatLng = new google.maps.LatLng(p.latitude, p.longitude);
                             if (p.name) {
@@ -61,19 +62,23 @@ $(function(){
                     });
 
                 });
-            if (!is_entry) {
-                zoomChangeBoundsListener = google.maps.event.addListener(map, 'bounds_changed', function(event) { this.setZoom(10) });
-                //setTimeout(function(){google.maps.event.removeListener(zoomChangeBoundsListener)}, 5000);
-            }
-            var bindInfoWindow = function(marker, map, infowindow, html) {
-            google.maps.event.addListener(marker, 'click', function() {
-                infowindow.setContent(html);
-                infowindow.open(map, marker);
-                });
-            };
+                if (!is_entry) {
+                    zoomChangeBoundsListener = google.maps.event.addListener(map, 'bounds_changed', function (event) {
+                        this.setZoom(10)
+                    });
+                    //setTimeout(function(){google.maps.event.removeListener(zoomChangeBoundsListener)}, 5000);
+                }
+                var bindInfoWindow = function (marker, map, infowindow, html) {
+                    google.maps.event.addListener(marker, 'click', function () {
+                        infowindow.setContent(html);
+                        infowindow.open(map, marker);
+                    });
+                };
 
 
-        })
-    }
-    google.maps.event.addDomListener(window, "load", initialize);
+            })
+        }
+
+        google.maps.event.addDomListener(window, "load", initialize);
+    });
 });
