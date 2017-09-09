110ea09e44120d1d956ef66b269ba07d3ca5c8b2
mattkohl <hlaford@gmail.com>
2017-09-09 18:12:05 +0100

    udpated tests

--- dictionary/tests/test_utils.py
+++ dictionary/tests/test_utils.py
@@ -1,6 +1,7 @@
 from django.test import TestCase
-from dictionary.models import Entry
-from dictionary.utils import slugify, extract_short_name, extract_parent, build_example, add_links, inject_link
+from dictionary.models import Entry, Artist, Song, LyricLink, Example, Place
+from dictionary.utils import slugify, extract_short_name, extract_parent, build_example, add_links, \
+    inject_link, swap_place_lat_long, format_suspicious_lat_longs, gather_suspicious_lat_longs
 
 
 class TestUtils(TestCase):
@@ -13,6 +14,49 @@ class TestUtils(TestCase):
 
     def test_extract_parent(self):
         self.assertEqual(extract_parent("Houston, Texas, USA"), "Texas, USA")
+
+
+class TestBuildExample(TestCase):
+
+    def setUp(self):
+        self.e = Example(
+            lyric_text="Now, it's time for me, the E, to rock it loco",
+            artist_name='EPMD',
+            artist_slug='EPMD',
+            song_title='Brothers From Brentwood L.I.',
+            album='Crossover',
+            release_date='1992-07-28',
+            release_date_string='1992-07-28'
+        )
+        self.e.save()
+        self.a = Artist(name='EMPD')
+        self.l1 = LyricLink(
+            link_text="E",
+            link_type="artist",
+            position=27,
+            target_lemma="E",
+            target_slug="erick-sermon"
+        )
+        self.l2 = LyricLink(
+            link_text='loco',
+            link_type='xref',
+            position=41,
+            target_lemma='loco',
+            target_slug='loco#e7360_adv_1'
+        )
+        self.l3 = LyricLink(
+            link_text='rock',
+            link_type='xref',
+            position=33,
+            target_lemma='rock',
+            target_slug='rock#e9060_trV_1'
+        )
+        self.l1.save()
+        self.l2.save()
+        self.l3.save()
+        self.e.lyric_links.add(self.l1)
+        self.e.lyric_links.add(self.l2)
+        self.e.lyric_links.add(self.l3)
 
     def test_build_example(self):
         pass
@@ -27,3 +71,27 @@ class TestUtils(TestCase):
         result = inject_link(lyric, start, end, a)
         expected = """Now, it's time for me, the <a href="/artists/erick-sermon">E</a>, to <a href="/rock#e9060_trV_1">rock</a> it <a href="/loco#e7360_adv_1">loco</a>"""
         self.assertEqual(result, expected)
+
+
+class TestPlaceMgmtUtils(TestCase):
+
+    def setUp(self):
+        self.lat = -1.23
+        self.lng = 2.34
+        self.p = Place(name="test", slug="test-usa", latitude=self.lat, longitude=self.lng)
+        self.p.save()
+
+    def test_swap_place_lat_long(self):
+
+        swap_place_lat_long(self.p)
+        self.assertEqual(self.p.latitude, self.lng)
+        self.assertEqual(self.p.longitude, self.lat)
+
+    def test_format_suspicious_lat_longs(self):
+        results = format_suspicious_lat_longs([self.p])
+        self.assertEqual(results[0], '0 -1.23\t2.34\ttest-usa')
+
+    def test_gather_suspicious_lat_longs(self):
+        suspects = gather_suspicious_lat_longs()
+        self.assertEqual(suspects.count(), 1)
+        self.assertEqual(suspects.first(), self.p)
