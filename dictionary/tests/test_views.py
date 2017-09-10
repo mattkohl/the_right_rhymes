0eb7e9f00afaa5754781c713154acdc16c90f09f
mattkohl <hlaford@gmail.com>
2017-09-10 06:45:39 +0100

    more template tests

--- dictionary/tests/test_views.py
+++ dictionary/tests/test_views.py
@@ -1,7 +1,17 @@
 from django.test import TestCase
+from dictionary.models import Entry, Artist, NamedEntity, Song, Domain, Place
 
 
 class TemplateTests(TestCase):
+    def setUp(self):
+        self.a = Artist(name="EPMD", slug="epmd")
+        self.a.save()
+        self.a1 = Artist(name="Erick Sermon", slug="erick-sermon")
+        self.a1.save()
+        self.p = Place(full_name="Brentwood, New York, USA", slug="brentwood-new-york-usa", name="Brentwood")
+        self.p.save()
+        self.d = Domain(name="drugs", slug="drugs")
+        self.d.save()
 
     def test_uses_index_template(self):
         response = self.client.get("/")
@@ -14,3 +24,20 @@ class TemplateTests(TestCase):
     def test_uses_about_template(self):
         response = self.client.get("/about-the-right-rhymes/")
         self.assertTemplateUsed(response, "dictionary/about.html")
+
+    def test_uses_artist_template(self):
+        response = self.client.get("/artists/epmd/")
+        self.assertTemplateUsed(response, "dictionary/artist.html")
+
+    def test_uses_place_template(self):
+        response = self.client.get("/places/brentwood-new-york-usa/")
+        self.assertTemplateUsed(response, "dictionary/place.html")
+
+    def test_uses_domain_template(self):
+        response = self.client.get("/domains/drugs/")
+        self.assertTemplateUsed(response, "dictionary/domain.html")
+
+    def test_uses_domains_template(self):
+        response = self.client.get("/domains/")
+        self.assertTemplateUsed(response, "dictionary/domains.html")
+
