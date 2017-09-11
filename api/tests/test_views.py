b9fe83ca5839949330e8f8b02cd922b071eaf136
mattkohl <hlaford@gmail.com>
2017-09-11 08:05:08 +0200

    last view test

--- api/tests/test_views.py
+++ api/tests/test_views.py
@@ -1,3 +1,4 @@
+from unittest import mock
 from django.test import TestCase
 from dictionary.models import Artist, Place
 
@@ -8,6 +9,26 @@ class TestArtistEndpoints(TestCase):
         self.artist = Artist(name="EPMD", slug="epmd")
         self.artist.save()
 
-    def test_artist_get(self):
+    @mock.patch('dictionary.utils.check_for_image')
+    def test_artist_get(self, mock_check_for_image):
+        mock_check_for_image.return_value = 'some_image.png'
         result = self.client.get("/data/artists/epmd", follow=True)
+        expected = {
+            'user': 'AnonymousUser',
+            'artists': [{'image': 'some_image.png', 'name': 'EPMD', 'slug': 'epmd'}],
+            'auth': 'None'
+        }
         self.assertEqual(result.status_code, 200)
+        self.assertDictEqual(result.json(), expected)
+
+    @mock.patch('dictionary.utils.check_for_image')
+    def test_artists_get(self, mock_check_for_image):
+        mock_check_for_image.return_value = 'some_image.png'
+        result = self.client.get("/data/artists/", follow=True)
+        expected = {
+            'user': 'AnonymousUser',
+            'artists': [{'image': 'some_image.png', 'name': 'EPMD', 'slug': 'epmd'}],
+            'auth': 'None'
+        }
+        self.assertEqual(result.status_code, 200)
+        self.assertDictEqual(result.json(), expected)
