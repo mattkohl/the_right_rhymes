1d27b777dfe08425bb4e733aaec1d9e904e784bd
mattkohl <hlaford@gmail.com>
2017-09-20 07:35:12 +0100

    added first trrentry test

--- dictionary/tests/test_xml_handler.py
+++ dictionary/tests/test_xml_handler.py
@@ -1,7 +1,7 @@
 from unittest import mock
 from collections import OrderedDict
 from dictionary.tests.base import BaseXMLTest, BaseTest
-from dictionary.management.commands.xml_handler import XMLDict, TRRDict
+from dictionary.management.commands.xml_handler import XMLDict, TRRDict, TRREntry
 
 
 class TestXMLDict(BaseXMLTest):
@@ -40,3 +40,23 @@ class TestTRRDict(BaseTest):
         self.assertEqual(result.entry_count, 0)
         self.assertEqual(str(result), "Python dict representation of The Right Rhymes. Entry count: 0")
 
+    def test_print_stats(self):
+        result = TRRDict({"dictionary": {"entry": []}})
+        self.assertEqual(str(result), "Python dict representation of The Right Rhymes. Entry count: 0")
+
+
+class TestTRREntry(BaseTest):
+
+    entry_dict = {
+            'senses': [],
+            'head': {'headword': 'zootie'},
+            '@sk': 'zootie',
+            '@publish': 'yes',
+            '@eid': 'e11730'
+        }
+
+    @mock.patch("dictionary.management.commands.xml_handler.TRREntry.update_entry")
+    def test_construct(self, mock_update_entry):
+        mock_update_entry.return_value = None
+        result = TRREntry(self.entry_dict)
+        self.assertEqual(str(result), 'zootie')
