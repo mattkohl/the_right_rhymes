a29ca55816739ed470259fe0f1228cec8573002f
mattkohl <hlaford@gmail.com>
2019-08-16 19:43:52 +0100

    fixed heatmap

--- dictionary/static/dictionary/js/maps.js
+++ dictionary/static/dictionary/js/maps.js
@@ -11,9 +11,7 @@ function initializeMapsDelay() {
 
         var plot = new mapboxgl.Map({
             container: 'map' + index,
-            style: 'mapbox://styles/mattkohl/cjzbh31rv0g3c1cpcos3l17xv',
-            center: [-98.5795, 39.8283],
-            zoom: 2
+            style: 'mapbox://styles/mattkohl/cjzbh31rv0g3c1cpcos3l17xv'
         });
 
         var isEntry = true;
@@ -39,35 +37,15 @@ function initializeMapsDelay() {
                 });
 
                 plot.addLayer({
-                    "id": "points-heat",
-                    "type": "heatmap",
+                    "id": "heatmap-12",
                     "source": "points",
-                    "maxzoom": 9,
-                    "paint": {
-                        // Increase the heatmap weight based on frequency and property magnitude
-                        "heatmap-weight": [
-                            "interpolate",
-                            ["linear"],
-                            ["get", "weight"],
-                            0, 0,
-                            6, 3
-                        ],
-                        // Increase the heatmap color weight weight by zoom level
-                        // heatmap-intensity is a multiplier on top of heatmap-weight
-                        "heatmap-intensity": [
-                            "interpolate",
-                            ["linear"],
-                            ["zoom"],
-                            0, 1,
-                            9, 3
-                        ],
-                        // Color ramp for heatmap.  Domain is 0 (low) to 1 (high).
-                        // Begin color ramp at 0-stop with a 0-transparancy color
-                        // to create a blur-like effect.
+                    "type": "heatmap",
+                    paint: {
+                        "heatmap-radius": 12,
                         "heatmap-color": [
-                            "interpolate",
-                            ["linear"],
-                            ["heatmap-density"],
+                        "interpolate",
+                        ["linear"],
+                        ["heatmap-density"],
                             0, "rgba(33,102,172,0)",
                             0.2, "rgb(103,169,207)",
                             0.4, "rgb(209,229,240)",
@@ -75,72 +53,6 @@ function initializeMapsDelay() {
                             0.8, "rgb(239,138,98)",
                             1, "rgb(178,24,43)"
                         ],
-                        // Adjust the heatmap radius by zoom level
-                        "heatmap-radius": [
-                            "interpolate",
-                            ["linear"],
-                            ["zoom"],
-                            0, 2,
-                            9, 20
-                        ],
-                        // Transition from heatplot to circle layer by zoom level
-                        "heatmap-opacity": [
-                            "interpolate",
-                            ["linear"],
-                            ["zoom"],
-                            7, 1,
-                            9, 0
-                        ],
-                    }
-                });
-
-                plot.addLayer({
-                    "id": "points-circles",
-                    "type": "circle",
-                    "source": "points",
-                    "paint": {
-                        // Size circle radius by earthquake magnitude and zoom level
-                        "circle-radius": [
-                            "interpolate",
-                            ["linear"],
-                            ["zoom"],
-                            7, [
-                                "interpolate",
-                                ["linear"],
-                                ["get", "weight"],
-                                1, 1,
-                                6, 4
-                            ],
-                            16, [
-                                "interpolate",
-                                ["linear"],
-                                ["get", "weight"],
-                                1, 5,
-                                6, 50
-                            ]
-                        ],
-                        // Color circle by earthquake magnitude
-                        "circle-color": [
-                            "interpolate",
-                            ["linear"],
-                            ["get", "weight"],
-                            1, "rgba(33,102,172,0)",
-                            2, "rgb(103,169,207)",
-                            3, "rgb(209,229,240)",
-                            4, "rgb(253,219,199)",
-                            5, "rgb(239,138,98)",
-                            6, "rgb(178,24,43)"
-                        ],
-                        "circle-stroke-color": "white",
-                        "circle-stroke-width": 1,
-                        // Transition from heatmap to circle layer by zoom level
-                        "circle-opacity": [
-                            "interpolate",
-                            ["linear"],
-                            ["zoom"],
-                            7, 0,
-                            8, 1
-                        ]
                     }
                 });
 
